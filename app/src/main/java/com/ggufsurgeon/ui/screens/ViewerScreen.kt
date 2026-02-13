7dbc6b167eba98df8a92adcf51c06a401176d46f
ddurkee651 <ddurkee651@gmail.com>
2026-02-12 19:11:35 -0500

    Update ViewerScreen.kt

--- app/src/main/java/com/ggufsurgeon/ui/screens/ViewerScreen.kt
+++ app/src/main/java/com/ggufsurgeon/ui/screens/ViewerScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import android.content.Intent
 import com.ggufsurgeon.domain.ModelFile
 import com.ggufsurgeon.domain.TensorInfo
 
